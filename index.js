9a45203b03068456dc277c77d49d7d9679253ede
Shiv Kumar <123741836+shivkumar44@users.noreply.github.com>
2024-05-29 23:18:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer","Java Developer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
